889b483d18e3a319e675bd2f01642ddce97b62a0
PanKake21 <104033809+PanKake21@users.noreply.github.com>
2025-04-27 20:14:07 -0400

    accessible-grocery-list

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (e) => {
+  console.log("Service Worker: Installed");
+  e.waitUntil(
+    caches.open("static").then((cache) => {
+      return cache.addAll(["./", "./index.html", "./manifest.json"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
